5bb851c55dd569a3d03d2c00c484f000d88cf677
kevin_loustau <kevin.loustau@gmail.com>
2019-01-26 11:22:15 +0100

    add electronjs

---
+++ renderer.js
@@ -0,0 +1,9 @@
+//const Tone = require('tone');
+//tone
+//var synth = new Tone.Synth().toMaster();
+//synth.triggerAttackRelease('C4', '8n');
+
+
+// try to use http://mpe.js.org
+const mpeInstrument = require('mpe');
+const instrument = mpeInstrument();
