5ec14df65e630281a80c4c281282b93d835a049a
kevin_loustau <kevin.loustau@gmail.com>
2019-01-26 12:37:01 +0100

    fix tree to threejs ...

--- renderer.js
+++ renderer.js
@@ -1,9 +1,29 @@
-//const Tone = require('tone');
+const Tone = require('tone');
+const THREE = require('three');
+
+
 //tone
 //var synth = new Tone.Synth().toMaster();
 //synth.triggerAttackRelease('C4', '8n');
 
+//three
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-// try to use http://mpe.js.org
-const mpeInstrument = require('mpe');
-const instrument = mpeInstrument();
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame( animate );
+  cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+	renderer.render( scene, camera );
+}
+animate();
